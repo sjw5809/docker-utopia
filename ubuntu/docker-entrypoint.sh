10d160e85bb2fbe8f29a1fd332e135479b5f4780
sjw5809 <44254609+sjw5809@users.noreply.github.com>
2020-02-27 16:10:53 +0800

    Update docker-entrypoint.sh

--- ubuntu/docker-entrypoint.sh
+++ ubuntu/docker-entrypoint.sh
@@ -5,6 +5,6 @@ cpulimit -l ${CPU_USAGE} -e utopia_bot &
 
 cd /root/utopia-bot* && screen -dmS crp ./utopia_bot --pk ${UTOPIA_PUBLIC_KEY}
 
-exec ~/docker-entrypoint.sh
+exec /usr/local/bin/docker-entrypoint.sh
 
 tail -f /dev/null
